5168b5583335c6bd158f8484419332d5e8a19c22
Moritz Nossek <moritz.nossek@imagination.at>
2016-07-15 17:39:36 +0200

    TravelLocation update

--- Virtual Tourist/TravelLocationsMapViewController.swift
+++ Virtual Tourist/TravelLocationsMapViewController.swift
@@ -7,29 +7,95 @@
 //
 
 import UIKit
+import MapKit
 
 class TravelLocationsMapViewController: UIViewController {
-
+    
+    //MARK: Outlets
+    
+    @IBOutlet weak var navigationButtonRight: UIBarButtonItem!
+    @IBOutlet weak var deleteView: UIView!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    //MARK: -
+  
+    //MARK: View Management
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        addLongPressGestureRecognizer()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //MARK: -
+    
+    //MARK: Button Management
+    
+    @IBAction func editButtonPressed(sender: UIBarButtonItem) {
+        toggleDeleteView()
     }
-    */
-
+    
+    private func toggleDeleteView() {
+        
+        guard let navigationButtonText = navigationButtonRight.title else {
+            debugPrint("There is no valid navigation button text available.")
+            return
+        }
+        
+        //switch over button text to identify the state
+        switch navigationButtonText {
+        case "Edit":
+            navigationButtonRight.title = "Done"
+            deleteView.hidden = false
+            break
+            
+        case "Done":
+            navigationButtonRight.title = "Edit"
+            deleteView.hidden = true
+            break
+        default:
+            break
+        }
+    }
+    
+    //MARK: -
+    
+    //MARK: MapView Management
+    
+    private func addLongPressGestureRecognizer() {
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(TravelLocationsMapViewController.selectorAddAnnotation))
+        mapView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    @objc private func selectorAddAnnotation(gestureRecognizer: UIGestureRecognizer) {
+        
+        switch gestureRecognizer.state {
+        case UIGestureRecognizerState.Began:
+            addAnnotation(gestureRecognizer.locationInView(mapView))
+            break
+        default:
+            break
+        }
+    }
+    
+    private func addAnnotation(touchPoint: CGPoint) {
+        let coordinates = mapView.convertPoint(touchPoint, toCoordinateFromView: mapView)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinates
+        
+        CLGeocoder().reverseGeocodeLocation(CLLocation(latitude: coordinates.latitude, longitude: coordinates.longitude), completionHandler: {
+            (placemarks, error) -> Void in
+            
+            guard error == nil else {
+                print("Reverse geocoder failed with error" + error?.localizedDescription)
+                return
+            }
+            
+            if placemarks?.count > 0 {
+                
+            }
+        })
+    }
+    
+    //MARK: -
+    
 }
